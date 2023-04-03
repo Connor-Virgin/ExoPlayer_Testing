ef93a85a67193a4587f8ed7004e1ce03a46c9740
yirud <yiru.ding@torontomu.ca>
2023-04-02 20:09:24 -0400

    LongArrayTest YD

--- library/common/src/main/java/com/google/android/exoplayer2/util/coe891_tests/LongArrayTest.java
+++ library/common/src/main/java/com/google/android/exoplayer2/util/coe891_tests/LongArrayTest.java
@@ -1,35 +1,56 @@
 package com.google.android.exoplayer2.util.coe891_tests;
 
 import com.google.android.exoplayer2.util.LongArray;
-import static org.junit.Assert.*;
-
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class LongArrayTest {
 
-  private LongArray longArray;
-
-  @Before
-  public void setUp() {
-    longArray = new LongArray();
-  }
-
   @Test
-  public void testAddAndGet() {
+  public void testAdd() {
+    LongArray longArray = new LongArray();
     longArray.add(1);
     longArray.add(2);
     longArray.add(3);
-
-    assertEquals(3, longArray.size());
-    assertEquals(1, longArray.get(0));
-    assertEquals(2, longArray.get(1));
-    assertEquals(3, longArray.get(2));
+    Assert.assertEquals(1, longArray.get(0));
+    Assert.assertEquals(2, longArray.get(1));
+    Assert.assertEquals(3, longArray.get(2));
   }
 
-  @Test(expected = IndexOutOfBoundsException.class)
-  public void testGetInvalidIndex() {
-    longArray.get(0); // This should throw an exception because the array is empty
+  @Test
+  public void testGet() {
+    LongArray longArray = new LongArray();
+    longArray.add(1);
+    longArray.add(2);
+    longArray.add(3);
+    Assert.assertEquals(1, longArray.get(0));
+    Assert.assertEquals(2, longArray.get(1));
+    Assert.assertEquals(3, longArray.get(2));
+    try {
+      longArray.get(3);
+      Assert.fail("Expected IndexOutOfBoundsException was not thrown");
+    } catch (IndexOutOfBoundsException e) {
+    }
   }
 
+  @Test
+  public void testSize() {
+    LongArray longArray = new LongArray();
+    Assert.assertEquals(0, longArray.size());
+    longArray.add(1);
+    Assert.assertEquals(1, longArray.size());
+    longArray.add(2);
+    Assert.assertEquals(2, longArray.size());
+  }
+
+  @Test
+  public void testToArray() {
+    LongArray longArray = new LongArray();
+    longArray.add(1);
+    longArray.add(2);
+    longArray.add(3);
+    long[] array = longArray.toArray();
+    Assert.assertArrayEquals(new long[]{1, 2, 3}, array);
+  }
 }
+
